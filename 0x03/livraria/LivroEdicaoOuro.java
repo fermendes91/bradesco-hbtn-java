13fdd3ad58ce674e52cd5cd385c19ad97d2b6a79
Fernando Mendes <fernando.mendes91@outlook.com>
2025-07-18 17:11:05 -0300

    Exceptions - parte 02 - exer 01 - fix01

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -4,7 +4,11 @@ public class LivroEdicaoOuro extends Livro{
 
 	public LivroEdicaoOuro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
 		super(titulo, autor, preco);
-		this.setPreco(preco * 1.3);
+	}
+
+	@Override
+	public double getPreco() {
+			return super.getPreco() * 1.3;
 	}
 
 }
