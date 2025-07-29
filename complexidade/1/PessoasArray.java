ca6bf0d3b06b9ae54331d6154f4566b87e8506e5
Fernando Mendes <fernando.mendes91@outlook.com>
2025-07-29 13:54:44 -0300

    complexidade - exer 02

---
+++ complexidade/1/PessoasArray.java
@@ -0,0 +1,35 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaLinear
+    public void buscaLinear(String nome){
+        boolean encontrado = false;
+        for (int i = 0; i < nomes.length; i++) {
+        	System.out.println("Passando pelo indice:" + i);
+        	if (nome == nomes[i]) {
+        		encontrado = true;
+            	System.out.println("Nome pesquisado é " + nomes[i] + " que está na posição " + i);
+        	}
+        }
+        
+        if(!encontrado) {
+        	throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
+    }
+}
