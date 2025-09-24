e46fb87d0ba5dd509388baad16e4fe4189be69e2
Fernando Mendes <fernando.mendes91@outlook.com>
2025-09-24 10:15:39 -0300

    Lambda Expressions - Exer 01

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,28 @@
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkup = 10.0;
+
+    public Supplier<Double> precoComMarkup = () -> 
+        preco + (preco * percentualMarkup / 100);
+
+    public Consumer<Double> atualizarMarkup = novoMarkup -> 
+        this.percentualMarkup = novoMarkup;
+
+    public Produto(double preco, String nome) {
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
